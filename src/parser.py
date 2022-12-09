0e321034ada9948250f3456999c4aa76036ced70
Ian Nery <ian.muxis@gmail.com>
2022-12-09 02:44:48 -0300

    deleted intermediary code

--- src/parser.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# check every folder of the cic folder
-for folder in os.listdir('toparse'):
-  # check every file of the folder if it has a file named en_US.html, if not, copy the pt_BR.html file to en_US.html
-  if not os.path.exists('toparse/' + folder + '/en_US.html'):
-    print('toparse/' + folder + '/en_US.html')
-    os.system('cp toparse/' + folder + '/pt_BR.html toparse/' + folder + '/en_US.html')
-
